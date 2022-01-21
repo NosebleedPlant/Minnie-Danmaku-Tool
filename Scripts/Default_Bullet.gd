15c7fbe88af8eb2d73aaaf22ad50da7ab38d575f
Yalmaz Abdullah <42461443+YalmazAbdullah@users.noreply.github.com>
2022-01-21 00:38:25 -0700

    more functionality to basic emitter
    added some more stuff like aiming and collision detection as well as some more granular control to it

--- Scripts/Default_Bullet.gd
+++ Scripts/Default_Bullet.gd
@@ -5,6 +5,10 @@ export var bullet_radius = 0
 export var bullet_acceleration = 100
 #GLOBALS
 var collided = false
+var life_time = 0
+var life_span = null
+var player_position
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -13,16 +17,35 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	move(delta)
-	
+	if(life_span):
+		age(delta)
+#	if(player_position):
+#		collision_detection(player_position)
+	return
+
 # Function that moves the bullet: Default is a straight line
 func move(delta):
 	var motionVector = Vector2.ONE
 	motionVector.x *= cos(self.rotation)*bullet_acceleration
 	motionVector.y *= sin(self.rotation)*bullet_acceleration
 	self.position += motionVector*delta
+	return
+
+func age(delta):
+	life_time+=delta
+	if(life_time>=life_span):
+		self.queue_free()
+	return
 
 func collision_detection(playerVec:Vector2):
 	#collsions do not use Area 2D to reduce processing cost if using differently shaped bullet please change this code
 	if(self.position.direction_to(playerVec)<=bullet_radius):
 		collided = true;
+		print("hit")
 	return
+
+func set_player_pos(playerVec:Vector2):
+	player_position = playerVec
+
+func test():
+	print("hi")
--- Scripts/Default_Emitter.gd
+++ Scripts/Default_Emitter.gd
@@ -4,71 +4,87 @@ extends Node2D
 var _Bullet = preload("res://Scene/Default_Bullet.tscn")
 
 #EDITABLE PARAMETERS:
-#transformation params:
-#export (float, EXP,-360,360)var starting_angle = 0 setget set_starting_angle
-export (float, EXP,-360,360)var rotation_rate = 1#rate of eimiter rotaiotn
+export (NodePath)var playerNodePath
+#-transformation params:
+export (float, EXP,-360,360)var rotation_rate = 0#rate of eimiter rotaiotn
 export (Vector2)var bullet_offset#offset from emitter origin
-#shot params:
+#-shot params:
 export (float)var spray_cooldown = 1#cooldown between shots
 export var spray_count = 1#bulletcount in a single spray
 export (float)var spread_width=0#spread width between bullets
 export (float, EXP,-360,360)var spread_angle=0 setget set_spread_angle#spread angle between bullets
-#modulate direction +-
-#modulate speed?
-#aiming params:
-export (bool)var aiming = false#aiming
-#export var aim_offset = 0#offset from player
-#export var aime_delay = 0#delay in player tracking
-#unified bullet params:
-#bullet lifespan
-#destruct on exit
-#bullet moves relative to emitter
+#-aiming params:
+export (bool)var aim_enabled = false
+export (Vector2)var aim_offset#offset from player
+export var aim_pause = 0#calls to player position per second
 
 #GLOBALS
-var time = spray_cooldown
+var parent:Node2D
+var shot_timer = spray_cooldown
 var shoot_period = 0
 var spread_enabled = false
-
+var player
+var aim_timer = 0
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	parent = self.get_parent()
 	if(spray_count > 1&&spread_angle!=0.0):
 		spread_enabled = true
+	if(playerNodePath):
+		player = get_node(playerNodePath)
+	else:
+		push_error("Player path required")
 	return
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	rotation(delta)
+	var player_position = player.position
+	
+	aim(delta,player_position) if aim_enabled else rotate(delta)
 	shoot(delta)
+	bullet_poll(player_position)
 	return
 
-func rotation(delta):
+func rotate(delta):
 	self.rotation += rotation_rate*delta
 	if(self.rotation_degrees >= 360 or self.rotation_degrees <= -360):
 		self.rotation_degrees = 0
 	return
 
-func shoot(delta):
-	time += delta
-	var childBullets = []
-	if(time>=spray_cooldown):
-		childBullets = instance_bullet(childBullets)
-		childBullets = set_bullet_position(childBullets)
-		childBullets = set_bullet_rotation(childBullets)
-		for bullet in childBullets:
-			self.get_parent().add_child(bullet)
-			bullet.add_to_group("bullet_pool")
-		time = 0
+#@TODO: lerp to position rather then snapping to it
+func aim(delta,player_position):
+	aim_timer += delta
+	if(aim_timer>=aim_pause):
+		look_at(player_position + aim_offset)
+		aim_timer =0
 	return
 
-func instance_bullet(childBullets):
+func shoot(delta):
+	shot_timer += delta
+	var childBullets = []
+	if(shot_timer>=spray_cooldown):
+		childBullets = instanceBullet(childBullets)
+		childBullets = positionBullet(childBullets)
+		childBullets = rotateBullet(childBullets)
+		for bullet in childBullets:
+			parent.add_child(bullet)
+			bullet.add_to_group("bullet_pool")
+		shot_timer = 0
+	return
+
+func bullet_poll(player_position):
+	parent.get_tree().call_group("bullet_pool","set_player_pos",player_position)
+
+func instanceBullet(childBullets):
 	for i in spray_count:
 		var bullet = _Bullet.instance()
+		bullet.add_to_group("bullets")
 		bullet.position = self.position+bullet_offset
 		bullet.rotation = self.rotation
 		childBullets.append(bullet)
 	return childBullets
 
-func set_bullet_position(childBullets):
+func positionBullet(childBullets):
 	if(spray_count > 1&&spread_width>0):
 		var spread = (spread_width/2)*-1
 		var spread_increment = spread_width/(spray_count-1)
@@ -79,7 +95,7 @@ func set_bullet_position(childBullets):
 			spread+=spread_increment
 	return childBullets
 
-func set_bullet_rotation(childBullets):
+func rotateBullet(childBullets):
 	if(spread_enabled):
 		var spread_angle_increment = spread_angle/(spray_count-1)
 		var curr_angle = (spread_angle/2)*-1
@@ -95,3 +111,7 @@ func set_spread_angle(degs):
 	else:
 		spread_enabled = false
 	return
+
+func set_player_path(path):
+	playerNodePath = path
+	player = get_node(playerNodePath)
