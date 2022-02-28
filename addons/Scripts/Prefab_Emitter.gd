05a73abd6e07fc6930ee3541a5c36985eba037db
Yalmaz Abdullah <42461443+NosebleedPlant@users.noreply.github.com>
2022-02-28 01:57:29 -0700

    small change to prefab

--- addons/Scripts/Prefab_Emitter.gd
+++ addons/Scripts/Prefab_Emitter.gd
@@ -5,11 +5,13 @@ extends "res://addons/Scripts/Abstract_Emitter.gd"
 ###############################################################################
 export (String, FILE) var emitter_data_file #file containg emitter data
 
-#loads emitter data when entering scene
+#loads emitter data, except the position data when entering scene
 #param:none
 #return:none
 func _ready():
+	var old_position = position
 	load_Emitter(emitter_data_file)
+	position = old_position
 
 ###############################################################################
 #_OVERRIDES:
